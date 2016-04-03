82df8f1dd096350b946a85b9eb9a46d46ca26c5b
Alex Cordero <Alex Cordero>
2016-04-03 11:50:55 -0600

    finished string parser

--- CacheSim/CacheSim.cpp
+++ CacheSim/CacheSim.cpp
@@ -19,58 +19,30 @@ int main(int argc, char* argv[])
 	char op;
 	unsigned long long int address;
 	unsigned int bytesize;
-	int wait = 0;
-	/*
-	ifstream myfile;
-	myfile.open("C:\\Users\\Alex\\ecen4593-sp16\\traces-short\\tr1");
-	string line;
-	if (myfile.is_open())
-	{
-		while (getline(myfile, line))
-		{
-			cout << line << endl;
-		}
-		myfile.close();
-	}
-	*/
-	//while (scanf_s("%c %Lx %d\n",&op, &address, &bytesize) == 3)
 	string input_line;
-	//ofstream myfile;
-	//myfile.open("C:\\Users\\Alex\\ecen4593-sp16\\traces-short\\trlog");
-	cout << "Stuck" << endl;
-	//%I64u needs to be changed when running in a linux environment
-	//cout << scanf("%c %I64u %u\n", &op, &address, &bytesize) << endl;
-	/*
-	while (scanf("%c %s %u\n", &op, &input_line, &bytesize) == 2) {
-		if (input_line.length() < 10)
-		{
-			break;
-		}
-		address = stoull(input_line);
-		cout << op << " " << address << " " << bytesize << endl;
-	}
-	*/
 	string new_string = "";
 	unsigned i = 0;
 	while (getline(cin, input_line))
 	{
-		//cout << "found" << endl;
 		op = input_line[0];
-		new_string = "";
-		for (i = 2; i < 15; i++)
+		new_string = "0X";
+		//gets address as string
+		for (i = 2; i < 14; i++)
 		{
 			new_string += input_line[i];
 		}
-		address = stoull(new_string);
+		//converts hex address string to unsigned long long int
+		address = stoull(new_string, nullptr, 16);
 		new_string = "";
-		while (!isspace(input_line[i]))
+		i++;
+		//grab bytesize
+		do
 		{
 			new_string += input_line[i];
 			i++;
-		}
-		//s to unsigned
+		}while (input_line[i] != '\0' && !isspace(input_line[i]));
 		bytesize = stoi(new_string);
-		cout << op << " " << address << " " << bytesize << endl;
+		cout << op << " " << hex << address << " " << bytesize << endl;
 	}
     return 0;
 }
