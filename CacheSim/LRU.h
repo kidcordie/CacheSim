96c00b29fa69a2ad8c1a19e606e4ddadafbf2254
Alex Cordero <Alex Cordero>
2016-04-13 12:48:59 -0600

    added LRU

---
+++ CacheSim/LRU.h
@@ -0,0 +1,20 @@
+#pragma once
+
+struct tagNode
+{
+	unsigned long long int tag = 0;
+	int valid = 0;
+	int dirty = 0;
+	tagNode* next = nullptr;
+};
+class LRU
+{
+public:
+	LRU(int ind_size, int assoc);
+	bool check_addr(unsigned long long int index, unsigned long long int in_tag, bool write);
+	void mov_tagNode(tagNode* current, tagNode* dummy);
+private:
+	void add_tagNode(tagNode* prev);
+	tagNode** lru_array;
+	tagNode* vic_dummy;
+};
